d4206ad0caa49b925a86af9286ec60572785007c
LukaKoporec <luka.koporec123@gmail.com>
2021-10-07 12:59:33 +0200

    nameThePlayers

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var randomDice1 = Math.floor(Math.random() * 6) +1;
 var randomDice2 = Math.floor(Math.random() * 6) +1;
+let player1 = prompt("Name the first player:");
+let player2 = prompt("Name the second player:");
 document.querySelector(".img1").setAttribute("src", "images/dice" + randomDice1 + ".png");
 document.querySelector(".img2").setAttribute("src", "images/dice" + randomDice2 + ".png");
 
@@ -8,10 +10,16 @@ function changeTitle() {
         document.querySelector("h1").innerHTML = "DRAW!";
 
     } else if (randomDice1 > randomDice2) {
-        document.querySelector("h1").innerHTML = "Player 1 Wins!";
+        document.querySelector("h1").innerHTML =  player1 + " Wins!";
     } else {
-        document.querySelector("h1").innerHTML = "Player 2 Wins!";
+        document.querySelector("h1").innerHTML = player2 + " Wins!";
     }
 }
 
+function playerName() {
+    document.getElementById("p1").innerHTML = player1;
+    document.getElementById("p2").innerHTML = player2;
+}
+
+playerName();
 changeTitle();
